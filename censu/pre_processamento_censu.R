1e1eb3a65cad1a866499f667e81d8e1c87da1aaf
AlissonPereiraFernandes <alissonfernandes2224@gmail.com>
2025-07-26 00:11:11 -0300

    subindo os folders

--- censu/pre_processamento_censu.R
+++ censu/pre_processamento_censu.R
@@ -1,7 +1,8 @@
+base_main = read.csv("census.csv")
 base = read.csv("census.csv")
 base$X = NULL
 
-# TRANSFORMANDO ATRIBUTOS CATEGÓRICAS EM NUMÉRICAS #
+# TRANSFORMANDO ATRIBUTOS CATEGÓRICAS EM NUMÉRICAS (encode) #
 
 table(base$sex) # contagem dos registros
 unique(base$sex) # verificas os valores dos atributos
@@ -17,3 +18,11 @@ base$relationship = factor(base$relationship, levels = c(' Husband', ' Not-in-fa
 base$race = factor(base$race, levels = c(' Amer-Indian-Eskimo', ' Asian-Pac-Islander', ' Black', ' Other', ' White'), labels = c(1, 2, 3, 4, 5))
 base$native.country = factor(base$native.country, levels = c(' Cambodia', ' Canada', ' China', ' Columbia', ' Cuba', ' Dominican-Republic', ' Ecuador', ' El-Salvador', ' England', ' France', ' Germany', ' Greece', ' Guatemala', ' Haiti', ' Holand-Netherlands', ' Honduras', ' Hong', ' Hungary', ' India', ' Iran', ' Ireland', ' Italy', ' Jamaica', ' Japan', ' Laos', ' Mexico', ' Nicaragua', ' Outlying-US(Guam-USVI-etc)', ' Peru', ' Philippines', ' Poland', ' Portugal', ' Puerto-Rico', ' Scotland', ' South', ' Taiwan', ' Thailand', ' Trinadad&Tobago', ' United-States', ' Vietnam', ' Yugoslavia'), labels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41))
 base$income = factor(base$income, levels = c(' <=50K', ' >50K'), labels = c(0, 1))
+
+# ESCALONAMENTO DOS ATRIBUTOS #
+base[, 1] = scale(base[, 1])
+base[, 3] = scale(base[, 3])
+base[, 5] = scale(base[, 5])
+base[, 11:13] = scale(base[, 11:13])   
+
+
